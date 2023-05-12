e105f067659495a8fe1265233899e1ed38930b49
Arthur Biasibetti <arthurbiasibettifarias@gmail.com>
2023-05-12 16:00:40 -0300

    ResponseEntity

--- src/main/java/pet/care/api/controller/ClientController.java
+++ src/main/java/pet/care/api/controller/ClientController.java
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import pet.care.api.client.Client;
 import pet.care.api.client.ClientInsertDTO;
 import pet.care.api.client.ClienteRepository;
 
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
@@ -20,12 +22,21 @@ public class ClientController {
 
     @PostMapping
     @Transactional
-    public void setClient(@RequestBody ClientInsertDTO data) {
-        repository.save(new Client(data));
+    public ResponseEntity<Client> setClient(@RequestBody ClientInsertDTO data, UriComponentsBuilder uriBuilder) {
+        Client client = repository.save(new Client(data));
+        var uri = uriBuilder.path("/clients/{id}").buildAndExpand(client.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(client);
     }
 
     @GetMapping
-    public Page<Client> getClients(@PageableDefault(size = 3) Pageable page) {
-        return repository.findAll(page);
+    public ResponseEntity<Page<Client>> getClients(@PageableDefault(size = 3) Pageable page) {
+        var pages = repository.findAll(page);
+        return ResponseEntity.ok(pages);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClient(@PathVariable int id) {
+        return ResponseEntity.ok(repository.getReferenceById(id));
     }
 }
--- src/main/java/pet/care/api/controller/PetController.java
+++ src/main/java/pet/care/api/controller/PetController.java
@@ -3,7 +3,9 @@ package pet.care.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import pet.care.api.client.Client;
 import pet.care.api.client.ClienteRepository;
 import pet.care.api.pet.Pet;
@@ -13,7 +15,7 @@ import pet.care.api.pet.PetRepository;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pet")
+@RequestMapping("/pets")
 public class PetController {
 
     @Autowired
@@ -24,18 +26,26 @@ public class PetController {
 
     @PostMapping
     @Transactional
-    public Pet setPet(@RequestBody @Valid PetInsertDTO data) {
+    public ResponseEntity<Pet> setPet(@RequestBody @Valid PetInsertDTO data, UriComponentsBuilder uriBuilder) {
         Client owner = clientRepository.getReferenceById(data.owner_id());
         Pet pet = new Pet(data.name(), owner);
         petRepository.save(pet);
 
-        return pet;
+        var uri = uriBuilder.path("/pets/{id}").buildAndExpand(pet.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(pet);
 
 
     }
 
     @GetMapping
-    public List<Pet> getPet() {
-        return petRepository.findAll();
+    public ResponseEntity<List<Pet>> getPets() {
+        return ResponseEntity.ok(petRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pet> getPet(@PathVariable int id) {
+        return ResponseEntity.ok(petRepository.findById(id).get());
+    }
+
 }
